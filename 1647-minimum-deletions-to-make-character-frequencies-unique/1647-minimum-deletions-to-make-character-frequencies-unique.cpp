1e460156381652b248875ca2f4486d1c55270f4e
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-22 08:24:40 +0530

    Time: 65 ms (69.44%), Space: 17.2 MB (96.08%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    int minDeletions(string s)
+    {
+        vector<int> freq(26,0);        
+        for (char c: s)
+            freq[c - 'a']++;
+
+        sort(freq.begin(), freq.end(), greater<int>());
+        
+        int deleteCount = 0;
+        int maxFreqAllowed = s.size();
+        for (int i=0; i<26; i++)
+        {
+            if (freq[i] > maxFreqAllowed)
+            {
+                deleteCount += freq[i] - maxFreqAllowed;
+                freq[i] = maxFreqAllowed;
+            }
+            maxFreqAllowed = max(0, freq[i]-1);
+        }
+        
+        return deleteCount;
+    }
+};
