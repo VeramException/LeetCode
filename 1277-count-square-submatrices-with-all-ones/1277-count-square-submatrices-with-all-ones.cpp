b04dcd2f7e8b888450de34e2c6cd05a501b5d169
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-22 12:17:38 +0530

    Time: 87 ms (61.11%), Space: 26.4 MB (6.86%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -16,9 +16,7 @@ class Solution
                 if (matrix[r][c] == 1)
                 {
                     if (r > 0 && c > 0)
-                    {
                         dp[r][c] += min ({dp[r-1][c], dp[r][c-1], dp[r-1][c-1]}) + 1;
-                    }
                     else
                         dp[r][c] = 1;
                 }
