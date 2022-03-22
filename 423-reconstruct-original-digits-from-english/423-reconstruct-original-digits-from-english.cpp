243c3fd7b1c189378ec984c3f105215183faee47
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-22 10:54:20 +0530

    Time: 29 ms (48.34%), Space: 8.9 MB (87.80%) - LeetHub

--- 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.cpp
+++ 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.cpp
@@ -9,26 +9,28 @@ class Solution
         
         vector<int> nums(10, 0);
         
+        // Set-1: Indepedent
         nums[0] = charFreq['z' - 'a'];
         nums[2] = charFreq['w' - 'a'];
         nums[4] = charFreq['u' - 'a'];
         nums[6] = charFreq['x' - 'a'];
-        nums[8] = charFreq['g' - 'a'];
+        nums[8] = charFreq['g' - 'a'];        
         
+        // Set-2: based on Set-1
         nums[1] = charFreq['o' - 'a'] - nums[0] - nums[2] - nums[4];
         nums[3] = charFreq['r' - 'a'] - nums[0] - nums[4];
-        nums[7] = charFreq['s' - 'a'] - nums[6];        
-
+        nums[7] = charFreq['s' - 'a'] - nums[6];
+        
+        // Set-3: based on Set-2
         nums[5] = charFreq['v' - 'a'] - nums[7];
         
+        // Set-4: based on Set-3
         nums[9] = charFreq['i' - 'a'] - nums[5] - nums[6] - nums[8];
         
         string res;
         for (int i=0; i<10; i++)
-        {
             while (nums[i] != 0 && nums[i]-- != 0)
                 res += to_string(i);
-        }
         return res;
     }
 };
