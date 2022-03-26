7610b1b1cb8be99310be0d4224a997a9156c9984
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-26 11:55:21 +0530

    Time: 40 ms (78.79%), Space: 27.7 MB (13.43%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -3,21 +3,8 @@ class Solution
 public:
     int search(vector<int>& nums, int target)
     {
-        int lo = 0;
-        int hi = nums.size()-1;
+        auto iter = std::lower_bound (nums.begin(), nums.end(), target);
         
-        while (lo <= hi)
-        {
-            int mid = (lo + hi)/2;
-            if (nums[mid] == target)
-                return mid;
-            
-            if (target > nums[mid]) 
-                lo = mid+1;
-            else
-                hi = mid-1;
-        }
-        
-        return -1;
+        return (iter != nums.end() && *iter == target)? (iter - nums.begin()): -1;
     }
 };
