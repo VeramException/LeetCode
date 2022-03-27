86bc426878a557933897d79fa9d8e76d0fc021a0
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-27 09:51:05 +0530

    Time: 224 ms (82.35%), Space: 53.8 MB (100.00%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -1,25 +1,51 @@
-class Solution {
-public:
-    vector<long long> kthPalindrome(vector<int>& queries, int intLength) {
-        vector<long long> ans;
-        int n = queries.size(), m = (intLength + 1) / 2;
-        long long s = m > 1 ? stoi(string(m - 1, '9')) : 0, l = stoi(string(m, '9')) - s;
-        for (int i = 0; i < n; i++)
+class Solution
+{
+    public:
+    vector<long long> kthPalindrome(vector<int>& queries, int length)
+    {   
+        vector<long long> res;
+        for (auto const q: queries)
+            res.push_back(getIthPalindrome(q, length));
+        return res;
+    }
+    
+    long long getIthPalindrome(int i, int length)
+    {
+        long long res;
+        
+        // We only think of half-number.
+        // i.e., for EVEN -> 123321,  we only think of first 3 digits. It can be from 99+1 to 999
+        // i.e., for ODD  -> 1234321, we only think of first 4 digits. It can be from 999+1 to 9999
+        
+        long long minVal = 0;
+        long long maxVal = 0;
+        
+        int minDigits = 0;
+        int maxDigits = 0;
+        
+        if (length%2 == 0)
         {
-            if (queries[i] > l)
-                ans.push_back(-1);
-            else
-            {
-                long long h = queries[i] + s;
-                string t = to_string(h), r = t;
-                reverse(r.begin(), r.end());
-                if (intLength % 2)
-                    t += r.substr(1);
-                else
-                    t += r;
-                ans.push_back(stoll(t));
-            }
+            minDigits = length/2 - 1;
+            maxDigits = length/2;
         }
-        return ans;
+        else
+        {
+            minDigits = length/2;
+            maxDigits = length/2 + 1;
+        }
+        
+        while (minDigits-- > 0)
+            minVal = minVal*10 + 9;
+        while (maxDigits-- > 0)
+            maxVal = maxVal*10 + 9;
+        
+        if (i > (maxVal - minVal))
+            return -1;
+        
+        string s1 = to_string(minVal + i);
+        string s2 = (length%2 == 0)? s1: s1.substr(0, length/2);        
+        reverse(s2.begin(), s2.end());
+                
+        return stoll(s1+s2);
     }
 };
