835cd2cfe89bd4b74c8253d8fdb44e793263825a
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-27 11:18:09 +0530

    Time: 164 ms (57.14%), Space: 121.1 MB (57.14%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -3,22 +3,14 @@ class Solution
     public:
     int minDeletion(vector<int>& nums)
     {
-        if (nums.size() == 0)
-            return 0;
-        
         int i    = 0;
         int j    = 0;
-        int skip = 0;
         int N    = nums.size();
         
         while (j < N)
         {
             nums[i++] = nums[j++]; // Write Even
-            while (j < N && nums[i-1] == nums[j])
-            {
-                j++;
-                skip++;
-            }
+            while (j < N && nums[i-1] == nums[j]) j++;
             if (j < N)
                 nums[i++] = nums[j++]; // Write Odd
         }
