229e8d7208df691535ef096a6e87502337218d8e
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-27 10:43:04 +0530

    Time: 399 ms (58.82%), Space: 53.7 MB (100.00%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -11,23 +11,16 @@ class Solution
     
     long long getkthPalindrome(int k, int length)
     {
-        // We only need to find the first half of the number. Then we can simple reverse the 1st half digits to get the 2nd half.
-        
-        // For example, 
-        // if you need 1st 4-digit palindrome -> the first half is 9+1 = "10", The 2nd half is reverse of it "01"
-        // if you need 3rd 4-digit palindrome -> the first half is 9+3 = "12", The 2nd half is reverse of it "21"
-        
-        // For even n-digits, we only need to think of first_half i.e., n/2
-        // Eg: 123321 -> 123, 321 (reverse of 123)
-        
-        // For even n-digits, we only need to think of first_half+1 , i.e., n/2 + 1
-        // Eg: 1234321 -> 1234, 321 (reverse of 123)
-        
-        // Limits of a n-digit palindrome
-        // Even for limits, we only should think of first half digits.
-        // Eg: if you need a 6 digit palindrome, the first 3 digits can be from 99+1 to 999   (minVal+1 -> maxVal)
-        // Eg: if you need a 7 digit palindrome, the first 4 digits can be from 999+1 to 9999 (minVal+1 -> maxVal)
+        // k = 35, lenth = 10 (Even)
+        // 1st half number = 9999+35 = 10034         // minVal = 9999+1, maxVal = 99999
+        // 2nd half number = reverse(10034) = 43001
+        // Number = 1003443001
 
+        // k = 35, lenth = 11 (Odd)
+        // 1st half number = 99999+35 = 100034       // minVal = 99999+1, maxVal = 999999
+        // 2nd half number = reverse(1003) = 3001
+        // Number = 10003430001
+        
         int minDigits = (length%2 == 0)? (length/2 - 1): (length/2);
         int maxDigits = (length%2 == 0)? (length/2): (length/2 + 1);
         
