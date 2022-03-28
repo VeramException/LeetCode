16058775a47aa66aa7b12e820b8132d98e0659ef
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-28 12:05:55 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (75.42%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -15,25 +15,15 @@ class Solution
             
             if (nums[mid] < target)
             {
-                // If middle is on right part, we have: nums[left] > nums[right] >= nums[middle].
-                // When middle is on right part and target is greater than nums[right] (means not in nums[middle .. right]),
-                // should search in left part.
                 if (nums[left] > nums[mid] && target > nums[right])
                     right = mid - 1;
-                // When (1) middle is on left part, (2) middle is on right part and target is in nums[middle ... right],
-                // continue search in right part.
                 else 
                     left = mid + 1;
             }
             else
             {
-                // If middle is on left part, we have: nums[middle] >= nums[left] > nums[right]
-                // When middle is on left part and target is smaller than nums[left] (means not in nums[left .. middle]),
-                // should search in right part.
                 if (nums[mid] > nums[right] && target < nums[left])
                     left = mid + 1;
-                // When (1) middle is on the right part, (2) middle is on left part and target is in nums[left .. middle],
-                // continue search in left part.
                 else 
                     right = mid - 1;
             }
