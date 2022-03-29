44b601a66164f1752595c9c77f05dc88bc428063
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-29 21:26:12 +0530

    Time: 6 ms (8.83%), Space: 6.1 MB (22.85%) - LeetHub

--- 246-strobogrammatic-number/246-strobogrammatic-number.cpp
+++ 246-strobogrammatic-number/246-strobogrammatic-number.cpp
@@ -3,17 +3,11 @@ class Solution
     public:
     bool isStrobogrammatic(string num)
     {
-        vector<int> m (10, -1);
-        m[0] = 0;
-        m[1] = 1;
-        m[6] = 9;
-        m[8] = 8;
-        m[9] = 6;
+        unordered_map<char, char> m = {{'0','0'},{'1','1'},{'6','9'},{'8','8'},{'9','6'}};
         
-        int N = num.size();
-        for (int i=0, j=N-1; i<=j; i++,j--)
+        for (int i=0, j=num.size()-1; i<=j; i++,j--)
         {
-            if (m[num[i]-'0'] != num[j]-'0')
+            if (m.find(num[i]) == m.end() || m[num[i]] != num[j])
                 return false;
         }
         
