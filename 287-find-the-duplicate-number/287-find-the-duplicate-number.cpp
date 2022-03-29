e9dd603405d6af8a91fe90be4be9e51afd58ed78
VeramException <93516745+VeramException@users.noreply.github.com>
2022-03-29 16:23:32 +0530

    Time: 102 ms (91.70%), Space: 61.2 MB (61.36%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+    int findDuplicate(vector<int>& nums)
+    {
+        int duplicate = -1;
+        for (int i=0; i<nums.size(); i++)
+        {
+            int cur = abs(nums[i]);
+            if (nums[cur] < 0)
+            {
+                return cur;
+            }
+            nums[cur] *= -1;
+        }
+        
+        return duplicate;
+    }
+};
