9b1ad2f648add7430be2dcc79ca77366280b1179
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-02 10:09:14 +0530

    Time: 404 ms (5.04%), Space: 73 MB (19.60%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution
 {
     public:
@@ -21,6 +22,37 @@ class Solution
             for (int j=n-1; j>=0; j--)
             {
                 if (text1[i] == text2[j])
+                {
+                    dp[i][j] = 1+dp[i+1][j+1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
+*/
+
+// This can be optimized to remove the extra space, and make space O(1)
+
+class Solution
+{
+    public:
+    int longestPalindromeSubseq(string s)
+    {
+        int n = s.size();
+        
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+        for (int i=n-1; i>=0; i--)
+        {
+            for (int j=n-1; j>=0; j--)
+            {
+                if (s[i] == s[n-j-1])
                 {
                     dp[i][j] = 1+dp[i+1][j+1];
                 }
