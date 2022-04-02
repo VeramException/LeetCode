9b01afe194e535df84355dc60e57744dd58f6296
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-02 08:19:09 +0530

    Time: 64 ms (67.98%), Space: 19.6 MB (87.28%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    bool validPalindrome(string s)
+    {
+        int l = 0;
+        int r = s.size()-1;
+        
+        return isValidPalindromeUtil(s, l, r, 0);
+    }
+    
+    bool isValidPalindromeUtil(string &s, int l, int r, int count)
+    {
+        if (count > 1)
+            return false;
+        
+        while (l < r)
+        {
+            if (s[l] != s[r])
+            {
+                return isValidPalindromeUtil(s, l+1, r, count+1) ||
+                       isValidPalindromeUtil(s, l, r-1, count+1);
+            }
+            l++, r--;
+        }
+        return true;
+    }
+};
