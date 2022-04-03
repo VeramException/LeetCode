ce2b209a546324e3588e239a4848d80913323669
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-03 11:11:22 +0530

    Time: 5 ms (50.00%), Space: 6.1 MB (16.67%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -4,10 +4,10 @@ class Solution
     int convertTime(string current, string correct)
     {
         int h1 = stoi(current.substr(0, 2));
-        int m1 = stoi(current.substr(3, 2));
+        int m1 = stoi(current.substr(3));
         
         int h2 = stoi(correct.substr(0, 2));
-        int m2 = stoi(correct.substr(3, 2));
+        int m2 = stoi(correct.substr(3));
         
         int diffMins = (h2*60+m2) - (h1*60+m1);        
         
