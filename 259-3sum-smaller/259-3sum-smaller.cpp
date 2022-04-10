0bb45a68ffa91b5ba17edbbda69c206f3ceeb7b0
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-10 17:11:42 +0530

    Time: 14 ms (97.38%), Space: 13.6 MB (14.21%) - LeetHub

---
+++ 259-3sum-smaller/259-3sum-smaller.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    int threeSumSmaller(vector<int>& nums, int target)
+    {
+        int count = 0;
+        
+        if (nums.size() < 3)
+            return count;
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i=0; i<nums.size()-2; i++)
+        {
+            if(nums[i]+nums[i+1]+nums[i+2]>=target)break;
+            
+            int j = i+1;
+            int k = nums.size()-1;
+            
+            while (j < k)
+            {
+                while (j<k && nums[i]+nums[j]+nums[k] >= target)
+                    k--;
+                 
+                count = count + (k-j);
+                j++;
+            }
+        }
+        
+        return count;
+    }
+};
