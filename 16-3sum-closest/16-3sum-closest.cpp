3b0ea31125e58088b7c7e39886a6eca32b9272b0
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-10 17:01:37 +0530

    Time: 21 ms (57.10%), Space: 10 MB (54.95%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -3,35 +3,38 @@ class Solution
     public:
     int threeSumClosest(vector<int>& nums, int target)
     {
-        if (nums.size() < 3)
+        // Edge case
+        if(nums.size() < 3)
             return 0;
         
-        sort (nums.begin(), nums.end());
+        int closest = nums[0] + nums[1] + nums[2];
         
-        int closest = nums[0] + nums[1] + nums[2];        
-        for (int i=0; i<nums.size()-2; i++)
+        // sort the numbers
+        sort(nums.begin(), nums.end());
+        
+        for(int first = 0 ; first < nums.size()-2 ; ++first)
         {
-            int j = i+1;
-            int k = nums.size()-1;
+            int second = first + 1;
+            int third  = nums.size() - 1;
             
-            // avoid duplicate comparisons
-            if (i > 0 && nums[i] == nums[i-1])
+            // to avoid wasted comparison.
+            if(first > 0 && nums[first] == nums[first-1])
                 continue;
             
-            while (j < k)
+            while (second < third)
             {
-                int currSum = nums[i] + nums[j] + nums[k];
+                int currSum = nums[first] + nums[second] + nums[third];
                 
                 if (currSum == target)
-                    return currSum;
+                    return target;
                 
-                if (abs(target-currSum) < abs(target-closest))
+                if (abs(target - currSum) < abs(target - closest))
                     closest = currSum;
                 
                 if (currSum > target)
-                    k--;
+                    third--;
                 else
-                    j++;
+                    second++;
             }
         }
         
