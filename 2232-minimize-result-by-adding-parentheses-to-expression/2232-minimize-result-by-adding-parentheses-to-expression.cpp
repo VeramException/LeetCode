998bb60a1f45ebe03bf8811aa5fa8d75e2071c5a
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-10 09:57:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

--- 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.cpp
+++ 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.cpp
@@ -10,25 +10,32 @@ public:
         int plus = exp.find('+');
         int N = exp.size();
 
-        // 123+4567
-        // 12(3+45)67
-
-        for (int i = 0; i < plus; i++)
+        // Step-1: Consider all possibilities,
+        //         and evaluate expression
+        for (int i = 0; i < plus; i++)           // Try '(' at 0 to plus-1 positions
         {
-            for (int j = plus + 1; j < N; j++)
+            for (int j = plus + 1; j < N; j++)   // Try ')' at plus+1 to N-1 positions
             {
+                // 12(3+45)67
+                // a = 12
+                // b = 3
+                // c = 45
+                // d = 67
+                
                 string a = (i == 0) ? "" : exp.substr(0, i);
-                string b = "(" + exp.substr(i, plus - i) + "+" + exp.substr(plus + 1, j - plus) + ")";
-                string c = (j == N - 1) ? "" : exp.substr(j+1, N - j + 1);
-                string newExp = a + b + c;
+                string b = exp.substr(i, plus - i);
+                string c = exp.substr(plus + 1, j - plus);
+                string d = (j == N - 1) ? "" : exp.substr(j+1, N - j + 1);
+                
+                string newExp = a + "(" + b + "+" + c + ")" + d;
 
-                int a_val = (i == 0) ? 1 : stoi(exp.substr(0, i));
-                int b_val = stoi(exp.substr(i, plus - i)) + stoi(exp.substr(plus + 1, j - plus));
-                int c_val = (j == N - 1) ? 1 : stoi(exp.substr(j + 1, N - j + 1));
+                int x = (a == "") ? 1 : stoi(a);
+                int y = stoi(b) + stoi(c);
+                int z = (d == "") ? 1 : stoi(d);
 
-                if (maxVal > a_val * b_val * c_val)
+                if (maxVal > x*y*z)
                 {
-                    maxVal =  a_val * b_val * c_val;
+                    maxVal =  x*y*z;
                     res = newExp;
                 }
             }
