452898e20dce5e8099cabb28f6c679268114fcbd
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-10 09:42:53 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (22.22%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -3,10 +3,10 @@ class Solution
 public:
     int largestInteger(int num)
     {
-        priority_queue<char> even;
-        priority_queue<char> odd;
         string s = to_string(num);
-        for (int i = 0; i < s.size(); i++)
+        priority_queue<char> even, odd;        
+        
+        for (int i=0; i<s.size(); i++)
         {
             if ((s[i]-'0') % 2 == 0)
                 even.push(s[i]);
@@ -15,7 +15,7 @@ public:
         }
 
         string res = s;
-        for (int i = 0; i < res.size(); i++)
+        for (int i=0; i<res.size(); i++)
         {
             if ((s[i] - '0') % 2 == 0)
                 s[i] = even.top(), even.pop();
@@ -23,8 +23,6 @@ public:
                 s[i] = odd.top(), odd.pop();
         }
 
-        int maxVal;
-        maxVal = stoi(s);
-        return maxVal;
+        return stoi(s);
     }
 };
