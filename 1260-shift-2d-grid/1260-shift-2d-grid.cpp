cde7867d8ca23ee45ba8e8d56341d4ff9789835a
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-11 11:28:24 +0530

    Time: 32 ms (71.23%), Space: 13.9 MB (73.22%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,20 +1,30 @@
 class Solution
 {
     public:
-vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-    int m = grid.size();
-    int n = grid[0].size();
-
-    k = k % (m*n);
-    while(k--)
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k)
     {
-       for(int i=0;i<m;++i)
-            for(int j=n-2;j>=0;--j)
-                swap(grid[i][j],grid[i][j+1]);
-
-       for(int i=m-2;i>=0;--i)
-            swap(grid[i][0],grid[i+1][0]);
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        // -- Optimization -- (Edge case)
+        // After rows*cols shifts, the result grid will be same as original input grid.
+        if (k % (rows*cols) == 0)
+            return grid;
+        
+        vector<vector<int>> res(rows, vector<int>(cols, 0));
+        for (int r=0; r<rows; r++)
+        {
+            for (int c=0; c<cols; c++)
+            {
+                int oldlinearIndex    = r*cols + c;
+                int newLinearIndex = (oldlinearIndex + k)%(rows*cols); // %(rows*cols) is important optimization.
+                
+                int rr = newLinearIndex/cols;
+                int cc = newLinearIndex%cols;
+                
+                res[rr][cc] = grid[r][c];
+            }
+        }
+        return res;
     }
-    return grid;
-}
 };
