59ce3423b06e5ffaad34772371d57712cc3885b7
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-13 19:43:22 +0530

    Time: 620 ms (74.92%), Space: 85.9 MB (88.17%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
@@ -1,24 +1,30 @@
+// Core logic:
+// At every iteration, increment the smallest number in the array by '1',
+// then the product will be maximum for that iteration
+// If we have 'k' increments to do, we just have to do the same thing for 'k' iterations.
+// i.e., find the smallest at every iteration and increment it by '1'
+
 class Solution
 {
     public:
     int maximumProduct(vector<int>& nums, int k)
     {
-        priority_queue <int, vector<int>, greater<int>> minHeap(nums.begin(), nums.end());
+        priority_queue <int, vector<int>, greater<int>> pq(nums.begin(), nums.end()); // minHeap
         
         while (k > 0)
         {
-            int x = minHeap.top(); minHeap.pop();
-            minHeap.push(x+1);
+            int x = pq.top(); pq.pop();
+            pq.push(x+1);
             k--;
         }
         
         long long res = 1;
         int mod = pow(10,9)+7;
-        while (!minHeap.empty())
+        while (!pq.empty())
         {
-            int x = minHeap.top();
+            int x = pq.top();
             res = (res * x) % mod;
-            minHeap.pop();
+            pq.pop();
         }
         return res;
     }
