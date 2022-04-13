2869e55b74483439640656878b3873ec81b66b71
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-13 07:43:20 +0530

    Time: 0 ms (100.00%), Space: 7 MB (30.92%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+    public:
+    int rows, cols;
+    
+    vector<int> spiralOrder(vector<vector<int>>& matrix)
+    {
+        if (matrix.size() == 1)
+            return matrix[0];
+        
+        rows = matrix.size();
+        cols = matrix[0].size();
+        
+        vector<int> res;
+        int startIndex = 0;
+        while (res.size() < rows*cols)
+            loadFrame(matrix, startIndex++, res);
+
+        return res;
+    }
+    
+    void loadFrame(vector<vector<int>>& matrix, int si, vector<int>& res)
+    {
+        // si = start index
+        
+        // Only one element remaining
+        if (res.size() == (rows*cols)-1)
+            res.push_back(matrix[si][si]);
+        
+        // Left to Right
+        // [si,si] -> [si, cols-si-1]
+        int r = si;
+        int c = si;
+        while (c < cols-si-1 && res.size() < rows*cols)
+            res.push_back(matrix[r][c++]);
+        
+        // Top to Bottom
+        r = si;
+        c = cols-si-1;
+        while (r < rows-si-1 && res.size() < rows*cols)
+            res.push_back(matrix[r++][c]);
+        
+        // Right to Left
+        r = rows-si-1;
+        c = cols-si-1;
+        while (c >= si+1 && res.size() < rows*cols)
+            res.push_back(matrix[r][c--]);
+        
+        // Bottom to Right
+        r = rows-si-1;
+        c = si;
+        while (r >= si+1 && res.size() < rows*cols)
+            res.push_back(matrix[r--][c]);
+    }
+};
