00b8ec1c452fd459d00a87c8d809a7c30f0964d2
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-15 00:09:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.53%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.cpp
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.cpp
@@ -5,22 +5,30 @@ class Solution
     
     int daysBetweenDates(string date1, string date2)
     {
-        return abs(daysFrom1971(date1)-daysFrom1971(date2));
+        return ((date1 > date2)? daysBetweenDatesUtil(date2, date1): daysBetweenDatesUtil(date1, date2));
     }
     
-    int daysFrom1971(string date)
+    int daysBetweenDatesUtil(string date1, string date2)
     {
+        int y1 = stoi(date1.substr(0,4));
+        return daysFromLastYear(date2, y1-1) - daysFromLastYear(date1, y1-1);
+    }
+    
+    int daysFromLastYear(string date, int fromYear)
+    {
+        vector<int> daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
         int y = stoi(date.substr(0,4));
         int m = stoi(date.substr(5,2));
         int d = stoi(date.substr(8,2));
         
-        for (int iy=1971; iy<y; iy++)
+        for (int iy=fromYear; iy<y; iy++)
             d += isLeapYear(iy)? 366: 365;
-
-        for (int im=0; im<m-1; im++)
-            d += daysOfMonth[im];
         
-        if (m > 2 && isLeapYear(y))
+        for (int im=0; im<m-1; im++)
+            d += daysInMonth[im];
+        
+        if (m>2 && isLeapYear(y))
             d++;
         
         return d;
