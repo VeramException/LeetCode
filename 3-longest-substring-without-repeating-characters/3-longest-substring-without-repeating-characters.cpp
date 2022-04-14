c8c048576010de1d3fecf4c83f60a89e61a3c843
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-14 06:22:43 +0530

    Time: 29 ms (43.16%), Space: 10.7 MB (31.02%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,7 +3,7 @@ class Solution
     public:
     int lengthOfLongestSubstring(string s)
     {
-        unordered_set<char> set; 
+        unordered_set<char> visited; // O(1) - Because it contains only a maximum of 256 characters
                 
         int l = 0;
         int r = 0;
@@ -13,15 +13,15 @@ class Solution
         
         while(r<n)
         {
-            if(set.find(s[r]) == set.end())
+            if(visited.count(s[r]) == 0)
             {
-                set.insert(s[r]);
+                visited.insert(s[r]);
                 maxDistance = max(maxDistance, r-l+1);
                 r++;
             }
             else
             {
-                set.erase(s[l]);
+                visited.erase(s[l]);
                 l++;
             }
         }    
