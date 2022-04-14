f8067dfa137141060a2c16d6d715db1a1989378a
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-14 06:08:47 +0530

    Time: 51 ms (62.24%), Space: 34.9 MB (24.22%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -1,17 +1,23 @@
+
+
+//
+// (2) - Iterative Solution
+//
 class Solution
 {
     public:
     TreeNode* searchBST(TreeNode* root, int target)
     {
-        if (!root)
-            return NULL;
-        
-        if (target == root->val)
-            return root;
-        
-        if (target < root->val)
-            return searchBST(root->left, target);
-        else
-            return searchBST(root->right, target);
+        while (root != NULL)
+        {
+            if (target == root->val)
+                return root;
+            
+            if (target < root->val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+        return root;
     }
 };
