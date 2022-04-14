6a58fdfa5ffb466b76a8ff058ab861e02f0e57c8
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-14 06:00:00 +0530

    Time: 45 ms (25.62%), Space: 10.9 MB (25.15%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -8,13 +8,14 @@ class Solution
         int l=0, r=0, maxLength=0;
         while (r < s.size())
         {
-            if (m.count(s[r]) == 1)
+            if (m.count(s[r]) > 0)
             {
                 while (l <= m[s[r]])
                     m.erase(m.find(s[l++]));
             }
-            m[s[r]] = r;
+            
             maxLength = max(maxLength, r-l+1);
+            m[s[r]] = r;
             r++;
         }
         
