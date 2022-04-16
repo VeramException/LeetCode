606b4297bbacd6d8fbf4b9306f65d3bf4d403c7c
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-16 11:25:49 +0530

    Time: 35 ms (91.20%), Space: 33.5 MB (19.45%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -3,14 +3,14 @@ class Solution
     public:
     TreeNode* convertBST(TreeNode* root)
     {
-        convertBSTUtil(root, 0);
+        int sum = 0;
+        convertBSTUtil(root, sum);
         return root;
     }
     
-    int convertBSTUtil(TreeNode* node, int sum)
+    int convertBSTUtil(TreeNode* node, int& sum)
     {
-        if (node == nullptr)
-            return sum;
+        if (node == nullptr)return sum;
      
         node->val += convertBSTUtil(node->right, sum);
         
