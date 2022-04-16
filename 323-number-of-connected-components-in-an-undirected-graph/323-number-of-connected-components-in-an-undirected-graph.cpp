87088d0a1332462cfd81a8714354532e1df1c647
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-16 21:41:53 +0530

    Time: 26 ms (61.80%), Space: 12.9 MB (51.67%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,51 @@
+// Union-Find
+
+class Solution
+{
+    public:
+    vector<int> parent;
+    vector<int> rank;
+    
+    int countComponents(int n, vector<vector<int>>& edges)
+    {        
+        for (int i=0; i<n; i++)
+        {
+            parent.push_back(i);
+            rank.push_back(1);
+        }
+        
+        int res = n;
+        for (auto edge: edges)
+            res = res - unions(edge[0], edge[1]);
+        return res;
+    }
+    
+    int find(int x)
+    {
+        int res = x;
+        while (res != parent[res])
+            res = parent[res];
+        return res;
+    }
+    
+    int unions(int x1, int x2)
+    {
+        int p1 = find(x1);
+        int p2 = find(x2);
+        
+        if (p1 == p2)
+            return 0;
+        
+        if (rank[p2] > rank[p1])
+        {
+            parent[p1] = p2;
+            rank[p2] += rank[p1];
+        }
+        else
+        {
+            parent[p2] = p1;
+            rank[p1] += rank[p2];
+        }        
+        return 1;
+    }
+};
