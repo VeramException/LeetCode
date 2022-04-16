7b3faa530a4fd6e7e8e6e33a089f809fa41d2a27
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-16 21:57:09 +0530

    Time: 20 ms (85.86%), Space: 12.9 MB (51.67%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -4,23 +4,23 @@ class Solution
 {
     public:
     vector<int> parent;   // {0, 1, 2, 3, 4, 5, ....... n}
-    vector<int> rank;     // {1, 1, 1, 1, 1, 1, ........1}
+    vector<int> size;     // {1, 1, 1, 1, 1, 1, ........1}
     int countComponents(int n, vector<vector<int>>& edges)
     {   
-        // Initialize parent & rank.
+        // Initialize parent & size.
         for (int i=0; i<n; i++)
         {
             parent.push_back(i);
-            rank.push_back(1);
+            size.push_back(1);
         }
         
-        int res = n;
+        int components  = n;
         for (auto edge: edges)
-            res = res - unions(edge[0], edge[1]);
-        return res;
+            components = components - unions(edge[0], edge[1]);
+        return components ;
     }
     
-    int find(int x)
+    int findParent(int x)
     {
         int res = x;
         while (res != parent[res])
@@ -33,21 +33,21 @@ class Solution
     
     int unions(int x1, int x2)
     {
-        int p1 = find(x1);        // find the top-most parent of x1
-        int p2 = find(x2);        // find the top-most parent of x2
+        int p1 = findParent(x1);  // find the top-most parent of x1
+        int p2 = findParent(x2);  // find the top-most parent of x2
         
         if (p1 == p2)             // if both have same parent, nothing needs to be done.
             return 0;
         
-        if (rank[p2] > rank[p1])  // if p2 have more children than p1, add p1 as child to p2 (to not increase tree height)
+        if (size[p2] > size[p1])  // if p2 have more children than p1, add p1 as child to p2 (to not increase tree height)
         {
             parent[p1] = p2;
-            rank[p2] += rank[p1]; 
+            size[p2] += size[p1]; 
         }
         else                      // else, do the oppposite
         {
             parent[p2] = p1;
-            rank[p1] += rank[p2];
+            size[p1] += size[p2];
         }        
         return 1;
     }
