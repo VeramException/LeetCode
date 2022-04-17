af11a293afd3837455c86da9e054f14164818fb8
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-17 14:16:29 +0530

    Time: 47 ms (65.27%), Space: 89.3 MB (64.25%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,26 @@
 class Solution
 {
     public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
-    {   
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+    double findMedianSortedArrays(vector<int>& a1, vector<int>& a2) {
+        if (a1.size() > a2.size()) swap(a1, a2); // make sure a1 is shorter
         
-        if (n1 < n2)
-            return findMedianSortedArrays(nums2, nums1); // nums2 should have less# elements
+        int n1 = a1.size(), n2 = a2.size();
         
-        int lo = 0;
-        int hi = n2*2;        
-        while (lo <= hi)
-        {
-            int cut2 = (lo+hi)/2;
-            int cut1 = (n1+n2) - cut2;
+        // range of a1 cut location: n1 means no right half for a1
+        int lo = 0, hi = n1;
+        while (lo <= hi) {
+            int cut1 = (lo + hi)/2; // cut location is counted to right half
+            int cut2 = (n1 + n2)/2 - cut1;
             
-            double l1 = (cut1 == 0)? INT_MIN: nums1[(cut1-1)/2];
-            double l2 = (cut2 == 0)? INT_MIN: nums2[(cut2-1)/2];
+            int l1 = cut1 == 0? INT_MIN : a1[cut1-1];
+            int l2 = cut2 == 0? INT_MIN : a2[cut2-1];
+            int r1 = cut1 == n1? INT_MAX : a1[cut1];
+            int r2 = cut2 == n2? INT_MAX : a2[cut2];
             
-            double r1 = (cut1 == n1*2)? INT_MAX: nums1[cut1/2];
-            double r2 = (cut2 == n2*2)? INT_MAX: nums2[cut2/2];
-            
-            // This condition should be satisified: l1 < r2 && l2 < r1)
-            if (l1 > r2)
-                lo = cut2+1;
-            else if (l2 > r1)
-                hi = cut2-1;
-            else
-                return (max(l1,l2)+min(r1,r2))/2;
+            if (l1 > r2) hi = cut1-1;
+            else if (l2 > r1) lo = cut1+1;
+            else return (n1+n2)%2? min(r1,r2) : (max(l1,l2) + min(r1,r2))/2.;
         }
-        
         return -1;
     }
 };
