a2c9e68f25d8e17e1193a58d13f523820d3542bc
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-17 14:50:33 +0530

    Time: 3 ms (65.44%), Space: 7.7 MB (77.99%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -1,27 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution
 {
     public:
-    TreeNode* increasingBST(TreeNode* root, TreeNode* tail = NULL)
+    TreeNode* increasingBST(TreeNode* root)
     {
-        if (root == nullptr)
-            return tail;
+        TreeNode* head = new TreeNode(0);
+        TreeNode* pre = head;
         
-        TreeNode* res = increasingBST(root->left, root);
+        stack<TreeNode*> s;
+        while (root || !s.empty())
+        {
+            while (root)
+            {
+                s.push(root);
+                root = root->left;
+            }
+            
+            root = s.top(); s.pop();
+            pre->right = root;
+            pre = pre->right;
+            root->left = nullptr;
+            root = root->right;
+        }
         
-        root->left  = nullptr;
-        root->right = increasingBST(root->right, tail);
-        
-        return res;
+        return head->right;
     }
 };
