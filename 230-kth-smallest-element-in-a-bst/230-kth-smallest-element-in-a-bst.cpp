a12cafef47f3a98c1cb80cf60ac7ec84e559b78f
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-18 10:36:36 +0530

    Time: 20 ms (74.44%), Space: 24.2 MB (60.91%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    int kthSmallest(TreeNode* root, int k)
+    {
+        int res;
+        inOrder(root, k, res);
+        return res;
+    }
+    
+    void inOrder(TreeNode* node, int& k, int& res)
+    {
+        if (node == nullptr)
+            return;
+        
+        inOrder(node->left, k, res);
+               
+        if (--k == 0)
+        {
+            res = node->val;
+            return;
+        }
+        
+        inOrder(node->right, k, res);
+    }
+};
