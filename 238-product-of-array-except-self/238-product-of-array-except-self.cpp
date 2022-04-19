10a63cec520f25659f1f0732b94436556c4db6b4
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-19 16:23:31 +0530

    Time: 27 ms (74.60%), Space: 24 MB (81.66%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -4,6 +4,32 @@ class Solution
     vector<int> productExceptSelf(vector<int>& nums)
     {
         int N = nums.size();
+        vector<int> res(N, 1);
+        
+        int leftProduct = 1;
+        for (int i=1; i<N; i++)
+        {
+            leftProduct = leftProduct * nums[i-1];
+            res[i] = res[i] * leftProduct;
+        }
+        
+        int rightProduct = 1;
+        for (int i=N-2; i>=0; i--)
+        {
+            rightProduct = rightProduct * nums[i+1];
+            res[i] = res[i] * rightProduct;
+        }
+        
+        return res;
+    }
+};
+/*
+class Solution
+{
+    public:
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        int N = nums.size();
         vector<int> lp(N, 1);
         vector<int> rp(N, 1);
         
@@ -19,3 +45,4 @@ class Solution
         return rp;
     }
 };
+*/
