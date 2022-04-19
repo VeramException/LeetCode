a28632e73997bc73da58fb48bddb588f8e11105e
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-19 19:07:36 +0530

    Time: 56 ms (39.16%), Space: 19.1 MB (66.75%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,20 +1,15 @@
 class Solution
 {
-    public:
-    static bool customSort(vector<int> a, vector<int> b)
-    {
-        return a[0]<b[0];
-    }
-    
+    public:    
     vector<vector<int>> merge(vector<vector<int>>& intervals)
     {
-        vector<vector<int>> res;
         if (intervals.size() == 0)
-            return res;
+            return {};
         
         // Sort by first value
-        sort(intervals.begin(), intervals.end(), customSort);
+        sort(intervals.begin(), intervals.end());
         
+        vector<vector<int>> res;
         res.push_back(intervals[0]);
         for (int i=1; i<intervals.size(); ++i)
         {
