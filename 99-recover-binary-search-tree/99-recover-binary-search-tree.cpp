ed15b69963ccf6dabc33d05eb5d3ed65a1c4e648
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-19 16:38:56 +0530

    Time: 40 ms (65.49%), Space: 57.8 MB (91.32%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    private:
+    TreeNode* first = nullptr;
+    TreeNode* second = nullptr;
+    
+    TreeNode* prev = new TreeNode(INT_MIN);
+    
+    public:
+    void recoverTree(TreeNode* root)
+    {
+        recoverTreeUtil(root);
+        swap(first->val, second->val);
+    }
+    
+    // Inorder traversal
+    void recoverTreeUtil(TreeNode* node)
+    {
+        if (node == nullptr)
+            return;
+        
+        recoverTreeUtil(node->left);
+        
+        if (first == nullptr && prev->val > node->val)
+            first = prev;
+        
+        if (first != nullptr && prev->val > node->val)
+            second = node;
+        
+        prev = node;
+        
+        recoverTreeUtil(node->right);
+    }
+};
