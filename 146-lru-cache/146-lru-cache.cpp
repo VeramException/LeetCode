892bc3ef95e3a8c052d1aa242cdded585bbee4ee
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-21 22:03:02 +0530

    Time: 616 ms (46.98%), Space: 165.2 MB (69.24%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,106 +1,46 @@
-struct Node
-{
-    int key, val;
-    Node* prev;
-    Node* next;
-    Node (int x, int y): key(x), val(y), prev(nullptr), next(nullptr) { }
-};
-
 class LRUCache
 {
     public:
     
     int cache_size;
-    unordered_map<int, Node*> m;
+    list<pair<int, int>> li;
+    unordered_map<int, list<pair<int, int>>::iterator> m;
     
-    Node* first; // least used
-    Node* last;  // recently used
-    
-    LRUCache(int capacity)
+    LRUCache(int capacity) : cache_size(capacity) 
     {
-        first = nullptr;
-        last = nullptr;
-        cache_size = capacity;
+        
     }
     
     int get(int key)
     {
         if (m.count(key) > 0)
         {
-            put(key, m[key]->val); // Touch the key so that it is brought back to the first.
-            return m[key]->val;
+            li.splice(li.begin(), li, m[key]);  // Take "m[key]" from "li" and place it in the beginning of li.
+            return m[key]->second;
         }
         return -1;
     }
     
     void put(int key, int value)
     {
-        // key exists
-        if (m.count(key) > 0)
+        // Bring it to front, update the key
+        if (get(key) != -1)
         {
-            m[key]->val = value;
-            Node* node = m[key];
-            
-            // First Node
-            if (node == last)
-            {
-            }
-            else if (node == first)
-            {
-                // remove this node from first
-                first = first->next;
-                first->prev = nullptr;
-                
-                // add this node at the last
-                pushNodeToEnd(node);
-            }
-            else
-            {
-                // connect previous and next nodes.
-                node->prev->next = node->next;
-                node->next->prev = node->prev;
-                
-                // add this node at the last
-                pushNodeToEnd(node);
-            }
+            m[key]->second = value;
         }
-        // new key
+        // New key
         else
         {
-            Node* node = new Node(key, value);
-            m[key] = node;
+            if (m.size() == cache_size)
+            {
+                int deleteKey = li.back().first;
+                li.pop_back();
+                m.erase(deleteKey);
+            }
             
-            if (first == nullptr)
-            {
-                first = node;
-                last = node;
-            }
-            else
-            {
-                // add this node at the last
-                pushNodeToEnd(node);
-                
-                if (m.size() > cache_size)
-                {
-                    // remove first node
-                    m.erase(m.find(first->key));
-                    
-                    Node* toDelete = first;
-                    first = first->next;
-                    first->prev = nullptr;
-                    
-                    delete(toDelete);
-                }
-            }
+            li.push_front({key, value});
+            m[key] = li.begin();
         }
-    }
-    
-    void pushNodeToEnd(Node* node)
-    {
-        last->next = node;
-        node->prev = last;
-        node->next = nullptr;
-        last = node;
     }
 };
 
