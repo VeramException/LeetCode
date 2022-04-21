48ff4d7574f08af69485350eaced2dfc9f423eb4
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-21 18:47:54 +0530

    Time: 23 ms (20.75%), Space: 20.3 MB (26.91%) - LeetHub

---
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    int diameterOfBinaryTree(TreeNode* root)
+    {
+        int res = 0;
+        diameterOfBinaryTreeUtil (root, res);
+        return res;
+    }
+    
+    int diameterOfBinaryTreeUtil(TreeNode* root, int& res)
+    {
+        if (root == nullptr)
+            return 0;
+        
+        int leftHeight = diameterOfBinaryTreeUtil(root->left, res);
+        int rightHeight = diameterOfBinaryTreeUtil(root->right, res);
+        
+        res = max (res, leftHeight + rightHeight);
+        
+        return max (leftHeight, rightHeight) + 1;
+    }
+};
