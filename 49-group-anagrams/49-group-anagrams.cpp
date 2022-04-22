e86817bb47a3e3532310e2c83a349c8e2842c05b
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-22 19:40:51 +0530

    Time: 37 ms (79.65%), Space: 19.7 MB (72.26%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -7,7 +7,7 @@ class Solution
         for (string word: words)
         {
             string t = word;
-            sort(t.begin(), t.end());  // this can be improved from O(nLog(n)) to O(1) using counting sort
+            sort(t.begin(), t.end());  // this can be improved from O(nLog(n)) to O(n) using counting sort
             m[t].push_back(word);
         }
         
@@ -16,4 +16,20 @@ class Solution
             res.push_back(v);
         return res;
     }
+    
+    /* counting sort 
+    string countingSort(string word)
+    {
+        int count[26] = {0};
+        for (char c: word)
+            count[c-'a']++;
+        
+        string sortedWord;
+        for (int i=0; i<26; i++)
+            sortedWord += string(count[i], i+'a');
+        return sortedWord;
+    }
+    
+    */
 };
+
