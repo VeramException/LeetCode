cb918d25963db4632de10e5c8e8f91a4290cadbb
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-23 01:47:58 +0530

    Time: 45 ms (26.25%), Space: 6.2 MB (98.50%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,42 @@
+// Do Top-Down with cache ==> Memoization
+// Bottom-Up i.e., DP can be really dificult to come up in interview.
+
+class Solution
+{
+public:
+    string s;
+    string p;
+    
+    bool isMatch(string ss, string pp)
+    {
+        s = ss;
+        p = pp;
+        
+        // Top-Down - Memoization.
+        return dfs (0, 0);        
+    }
+    
+    bool dfs(int i, int j)
+    {
+        if (i >= s.size() && j >= p.size())
+            return true;
+        
+        if (j >= p.size())
+            return false;
+        
+        bool match = (i < s.size() && j < p.size() && 
+                     (s[i] == p[j] || p[j] == '.'));
+        
+        if ((j + 1) < p.size() && p[j+1] == '*')
+        {
+            return dfs(i, j+2) || (match && dfs(i+1, j));
+        }
+        
+        if (match)
+        {
+            return dfs(i+1, j+1);
+        }
+        
+        return false;
+    }
+};
