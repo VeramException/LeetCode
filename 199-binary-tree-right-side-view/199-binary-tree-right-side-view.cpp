bad582db4d4f254c077d6f11ba4813d3882ed088
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-23 22:44:55 +0530

    Time: 0 ms (100.00%), Space: 12 MB (68.82%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -1,30 +1,78 @@
-class Solution
+// *********************
+// ******** BFS ********
+// *********************
+/*class Solution 
 {
     public:
-    vector<int> rightSideView(TreeNode* root)
+    vector<int> rightSideView(TreeNode* root) 
     {
-        vector<int> res;
-        if (root == nullptr)
-            return res;
+        vector<int> result;
+        if (!root)
+            return result;
         
         queue<TreeNode*> q;
         q.push(root);
         while (!q.empty())
         {
-            int siz = q.size();
-            while (siz-- > 0)
+            int qSiz = q.size();
+            while (qSiz>0)
             {
                 TreeNode* node = q.front(); q.pop();
                 
-                if (siz == 0)
-                    res.push_back(node->val);
+                if (qSiz == 1)
+                   result.push_back(node->val); 
                 
                 if (node->left)
                     q.push(node->left);
                 if (node->right)
                     q.push(node->right);
+                
+                qSiz--;
             }
+            
         }
+        return result;
+    }
+}; */
+
+// *********************
+// ******** DFS ******** O(1) space - nice solution
+// *********************
+class Solution
+{
+    public:
+    vector<int> res;
+    vector<int> rightSideView(TreeNode* root) 
+    {
+        dfs(root, 0);
         return res;
     }
+    
+    void dfs(TreeNode* node, int level)
+    {
+        if (node == nullptr)
+            return;
+        
+        if (res.size() == level)
+            res.push_back(node->val);
+        
+        dfs(node->right, level+1);
+        dfs(node->left, level+1);
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
