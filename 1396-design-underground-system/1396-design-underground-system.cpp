c420ea2ee7ab38f793aa65b7f2ff83695d03faeb
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-24 09:34:59 +0530

    Time: 154 ms (83.07%), Space: 57.6 MB (87.58%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -0,0 +1,36 @@
+
+class UndergroundSystem
+{
+    public:
+    
+    // <start-station, end-station, <sum, totalTrips>>
+    unordered_map<string, unordered_map<string,pair<int, int>>> tripStatistics; 
+    
+    // <id, <start-station, duration>>
+    unordered_map<int, pair<string, int>> activeTrips;   
+        
+    UndergroundSystem() { }
+    
+    void checkIn(int id, string startStation, int t)
+    {
+        activeTrips[id].first = startStation;
+        activeTrips[id].second = t;
+    }
+    
+    void checkOut(int id, string endStation, int t)
+    {
+        int duration = t - activeTrips[id].second;
+        tripStatistics[activeTrips[id].first][endStation].first += duration;
+        tripStatistics[activeTrips[id].first][endStation].second++;
+        
+        // erase when trip is ended.
+        activeTrips.erase(activeTrips.find(id));
+    }
+    
+    double getAverageTime(string startStation, string endStation)
+    {
+        int totalDuration = tripStatistics[startStation][endStation].first;
+        int totalTrips = tripStatistics[startStation][endStation].second;
+        return totalDuration/(double)totalTrips;
+    }
+};
