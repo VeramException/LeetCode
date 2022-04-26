f622991602e151a2684ec6ac23e286e7b32835f4
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-26 20:36:43 +0530

    Time: 6 ms (71.38%), Space: 10.2 MB (86.14%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -3,12 +3,14 @@ class Solution
     public:
     int arraySign(vector<int>& nums)
     {
-        int negCount = 0;
+        int sign = 1;
         
         for (int num: nums)
-            if (num == 0) return 0;
-            else if (num < 0) negCount++;
+            if (num == 0) 
+                return 0;
+            else if (num < 0)
+                sign *= -1;
          
-        return (negCount%2 == 0)? 1: -1;
+        return sign;
     }
 };
