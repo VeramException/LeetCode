e86aba38927e91157c747f03eccac39d72a12df7
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-26 12:03:35 +0530

    Time: 11 ms (50.28%), Space: 10.3 MB (99.79%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    int minSubArrayLen(int target, vector<int>& nums)
+    {        
+        int minLength = INT_MAX;
+        
+        int l = 0, sum = 0;
+        for (int r = 0; r < nums.size(); r++)
+        {
+            sum += nums[r];
+            
+            while (sum >= target && l <= r)
+            {
+                minLength = min (minLength, r-l+1);
+                sum = sum - nums[l];
+                l++;
+            }
+        }
+        
+        return (minLength == INT_MAX)? 0: minLength;
+    }
+};
