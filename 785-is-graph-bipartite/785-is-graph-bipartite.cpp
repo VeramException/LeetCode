2ab39c06d93ee13291cbb4c67a85f91341f8967d
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-29 09:45:44 +0530

    Time: 33 ms (49.69%), Space: 13.4 MB (93.88%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    vector<int> colors;
+    
+    bool isBipartite(vector<vector<int>>& graph)
+    {
+        // 0 --> Not coded
+        // 1 --> group A
+        // 2 --> group B
+        colors.resize(graph.size(), 0);
+        
+        for (int i=0; i<graph.size(); i++)
+            if (colors[i] == 0 && !isValidColorDfs(graph, 1, i))
+                return false;
+        
+        return true;
+    }
+    
+    bool isValidColorDfs(vector<vector<int>>& graph, int color, int node)
+    {
+        if (colors[node] != 0)
+            return colors[node] == color;
+        
+        colors[node] = color;
+        for (int j: graph[node])
+            if (!isValidColorDfs(graph, -color, j))
+                return false;
+        
+        return true;
+    }
+};
