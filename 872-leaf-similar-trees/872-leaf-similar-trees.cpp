3073c502847ef947250cf79e2a8ecff523a65caf
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-30 22:18:57 +0530

    Time: 0 ms (100.00%), Space: 12.8 MB (60.06%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.cpp
+++ 872-leaf-similar-trees/872-leaf-similar-trees.cpp
@@ -6,13 +6,13 @@ class Solution
     
     bool leafSimilar(TreeNode* root1, TreeNode* root2)
     {
-        inorderTraverse(root1, leaves_1);
-        inorderTraverse(root2, leaves_2);
+        dfs(root1, leaves_1);
+        dfs(root2, leaves_2);
      
         return (leaves_1 == leaves_2);
     }
     
-    void inorderTraverse(TreeNode* node, vector<int>& leaves)
+    void dfs(TreeNode* node, vector<int>& leaves)
     {
         if (node == nullptr)
             return;
@@ -20,9 +20,7 @@ class Solution
         if (node->left == nullptr && node->right == nullptr)
             leaves.push_back(node->val);
         
-        inorderTraverse(node->left, leaves);
-        inorderTraverse(node->right, leaves);
-        
-
+        dfs(node->left, leaves);
+        dfs(node->right, leaves);
     }
 };
