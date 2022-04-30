a8bbe4a159680302ae43d1bb014545663d843b56
VeramException <93516745+VeramException@users.noreply.github.com>
2022-04-30 08:03:36 +0530

    Time: 1606 ms (5.00%), Space: 234.3 MB (5.08%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,32 +1,15 @@
 class Solution
 {
     public:
-    struct customCompare
-    {
-        bool operator()(vector<int> a, vector<int> b)
-        {
-            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
-        }
-    };
     
+    static bool customCompare(vector<int> a, vector<int> b)
+    {
+        return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
+    }
     
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
     {
-        priority_queue<vector<int>, vector<vector<int>>, customCompare> pq;
-        
-        for (vector<int>& p: points)
-        {
-            pq.push(p);
-            if (pq.size() > k)
-                pq.pop();
-        }
-        
-        vector<vector<int>> res;
-        while (!pq.empty())
-        {
-            res.push_back(pq.top());
-            pq.pop();
-        }
-        return res;
+        sort(points.begin(), points.end(), customCompare);
+        return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
