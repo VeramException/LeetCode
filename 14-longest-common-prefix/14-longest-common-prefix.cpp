d5d7316d1c208dc21ae24831a4c45159615eedad
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-01 19:03:26 +0530

    Time: 6 ms (49.54%), Space: 9.2 MB (44.57%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -2,10 +2,9 @@ class Solution
 {
     public:
     string longestCommonPrefix(vector<string>& words)
-    {        
-        bool noMismatch = true;
-     
+    {     
         string res;
+        bool noMismatch = true;
         for (int i=0; i<words[0].size() & noMismatch; i++)
         {
             char c = words[0][i];                
@@ -13,10 +12,8 @@ class Solution
                 if (i >= words[j].size() || words[j][i] != c)
                     noMismatch = false;
 
-            if (noMismatch)
-                res += c;
+            if (noMismatch) res += c;
         }
-        
         return res;
     }
 };
