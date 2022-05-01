5a945f7b7026a8ffc709816b9939f55a2cbd5107
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-01 09:49:15 +0530

    Time: 105 ms (75.00%), Space: 10.6 MB (75.00%) - LeetHub

---
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    long long appealSum(string s)
+    {
+        long long res = 0;
+
+        // count the contribution of each character to the final sum
+        for (char c='a'; c<='z'; c++)
+            res += appealSumUtil(c, s);
+
+        return res;
+    }
+
+    long long appealSumUtil(char c, string& s)
+    {
+        long long N = s.size();
+        long long allCombs = N * (N+1)/2;
+          
+        long long count = 0;
+        long long currCombs = 0;
+
+        // Find the count of combinations that won't have c in them
+        for (int i=0; i<N; i++)
+        {
+            // Every time you get a c, calculate the number of combinations that are possible from previous 'count' characters.
+            // Note that the formula for 'how many combinations can we form with n characters' = n * (n+1)/2
+            if (s[i] == c)
+            {
+                currCombs += count*(count+1)/2;
+                count = 0;
+            }
+            else
+                count++;
+        }
+        
+        // Count the combinations for the last sub-string too
+        currCombs += count*(count+1)/2;
+        
+        // All combinations - (combinations that wont have c)
+        return allCombs - currCombs;
+    }
+};
