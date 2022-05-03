53c7c5af5316c9ada7719239b1391ece0793649e
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-03 11:38:28 +0530

    Time: 276 ms (17.50%), Space: 73.1 MB (23.02%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,13 +1,12 @@
-/*
-class Solution
+class Solution 
 {
     public:
     int longestPalindromeSubseq(string s)
     {
-        string text1 = s;        
-        string text2 = string(s.rbegin(), s.rend());
+        string s1 = s;
+        string s2 = s; reverse(s2.begin(), s2.end());
         
-        return longestCommonSubsequence(text1, text2);
+        return longestCommonSubsequence(s1, s2);
     }
     
     int longestCommonSubsequence(string text1, string text2)
@@ -15,54 +14,19 @@ class Solution
         int m = text1.size();
         int n = text2.size();
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        vector<vector<int>> DP(m+1, vector<int>(n+1, 0));
         
         for (int i=m-1; i>=0; i--)
         {
             for (int j=n-1; j>=0; j--)
             {
                 if (text1[i] == text2[j])
-                {
-                    dp[i][j] = 1+dp[i+1][j+1];
-                }
+                    DP[i][j] = 1+DP[i+1][j+1];
                 else
-                {
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
-                }
+                    DP[i][j] = max(DP[i+1][j], DP[i][j+1]);
             }
         }
         
-        return dp[0][0];
-    }
-};
-*/
-
-// This can be optimized to remove the extra space, and make space O(1)
-
-class Solution
-{
-    public:
-    int longestPalindromeSubseq(string s)
-    {
-        int n = s.size();
-        
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        
-        for (int i=n-1; i>=0; i--)
-        {
-            for (int j=n-1; j>=0; j--)
-            {
-                if (s[i] == s[n-j-1])
-                {
-                    dp[i][j] = 1+dp[i+1][j+1];
-                }
-                else
-                {
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
-                }
-            }
-        }
-        
-        return dp[0][0];
+        return DP[0][0];
     }
 };
