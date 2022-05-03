6c0d764f08d6bfc7721dc5a1d801673af83de0f0
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-03 16:03:57 +0530

    Time: 1762 ms (5.01%), Space: 234.3 MB (5.08%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -2,8 +2,6 @@ class Solution
 {
     public:
     
-    public:
-    
     static bool customCompare(vector<int> a, vector<int> b)
     {
         return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
@@ -12,6 +10,7 @@ class Solution
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
     {
         sort(points.begin(), points.end(), customCompare);
+        
         return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
