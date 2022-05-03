ad761e31d27c27a2096b0bf77ae4283365a8fac9
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-03 22:42:56 +0530

    Time: 3 ms (34.62%), Space: 6.3 MB (35.28%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -26,26 +26,13 @@ class Solution
         stringstream v1(version1);
         stringstream v2(version2);
         
-        int i = 3;
+        int i = 4;
         while (i-- > 0)
         {
             int isV1Greater = compare(v1, v2);
             if (isV1Greater == 1 || isV1Greater == -1)
                 return isV1Greater;
         }
-        
-        // MN
-        string d1, d2;
-        getline(v1, d1, '.');
-        getline(v2, d2, '.');
-        
-        int d1_i = (d1 == "")? 0: stoi(d1);
-        int d2_i = (d2 == "")? 0: stoi(d2);
-        
-        if (d1_i < d2_i)
-            return -1;
-        else if (d1_i > d2_i)
-            return 1;
          
         return 0;
     }
