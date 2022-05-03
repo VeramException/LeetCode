dc95df88b7488832e3c3cfa7da7026d59ebdf398
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-03 16:02:58 +0530

    Time: 1564 ms (5.01%), Space: 234.5 MB (5.08%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -13,3 +13,26 @@ class Solution
         return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
+
+// The above will sort all points O(n Log(n)).
+
+// But it is enough that we sort just first 'k' elements right? O(n Log(k))
+// Following is 'partial_sort' which will just sort first 'k' elements
+
+/*
+
+class Solution
+{
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
+    {
+         partial_sort(points.begin(), 
+                      points.begin() + k, 
+                      points.end(), 
+                      [](vector<int>& p, vector<int>& q) { return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1]; });
+                         
+         return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+};
+
+*/
