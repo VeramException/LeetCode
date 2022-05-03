c03c481cf62e36b7d619bed7cb91213df4eb8c74
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-03 11:41:48 +0530

    Time: 59 ms (17.71%), Space: 16.9 MB (12.46%) - LeetHub

---
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+    bool isValidPalindrome(string s, int k)
+    {
+        string s1 = s;
+        string s2 = s; reverse(s2.begin(), s2.end());
+        
+        int longestPalindromeSubsequence =  longestCommonSubsequence(s1, s2);
+        
+        return (longestPalindromeSubsequence + k >= s.size());
+    }
+    
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int m = text1.size();
+        int n = text2.size();
+        
+        vector<vector<int>> DP(m+1, vector<int>(n+1, 0));
+        
+        for (int i=m-1; i>=0; i--)
+        {
+            for (int j=n-1; j>=0; j--)
+            {
+                if (text1[i] == text2[j])
+                    DP[i][j] = 1+DP[i+1][j+1];
+                else
+                    DP[i][j] = max(DP[i+1][j], DP[i][j+1]);
+            }
+        }
+        
+        return DP[0][0];
+    }
+};
