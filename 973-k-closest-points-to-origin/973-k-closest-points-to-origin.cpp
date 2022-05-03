e5a563a8165959061e26c1f1bfe9f5256065682b
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-03 16:03:26 +0530

    Time: 1808 ms (5.01%), Space: 234.4 MB (5.08%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -2,6 +2,8 @@ class Solution
 {
     public:
     
+    public:
+    
     static bool customCompare(vector<int> a, vector<int> b)
     {
         return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
