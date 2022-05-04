be6b1079c4cf6b9bb2c2f0cbb1455ca6ecb5df86
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-04 12:04:26 +0530

    Time: 48 ms (94.59%), Space: 16.6 MB (72.66%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,30 +1,23 @@
-class Solution {
-public:
-
-#define NO_OF_CHARS 128
-
-int minSteps(string s1, string s2)
+class Solution
 {
-    int totalSteps = 0;
-    int count[NO_OF_CHARS] = {0};
-
-    for (int i = 0; i < s1.size(); i++)
-    {
-        count[s1[i]]++;
-    }
-
-    for (int i = 0; i < s2.size(); i++)
-    {
-        count[s2[i]]--;
-    }
-
-    for (int i = 0; i < NO_OF_CHARS; i++)
-    {
-        if (count[i] != 0)
-            totalSteps = totalSteps + abs(count[i]);
-    }
-
-    return totalSteps/2;
-}
+    public:    
+    #define NO_OF_CHARS 128
     
+    int minSteps(string s1, string s2)
+    {
+        int totalSteps = 0;
+        int count[NO_OF_CHARS] = {0};
+        
+        for (int i = 0; i < s1.size(); i++)
+            count[s1[i]]++;
+        
+        for (int i = 0; i < s2.size(); i++)
+            count[s2[i]]--;
+                
+        for (int i = 0; i < NO_OF_CHARS; i++)
+            if (count[i] != 0)
+                totalSteps = totalSteps + abs(count[i]);
+        
+        return totalSteps/2;
+    }
 };
