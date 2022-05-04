2e1447318f32e388369ace41bb26d80a93050604
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-04 11:23:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (55.87%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,9 +1,83 @@
 class Solution 
 {
-    public:
+public:
     
-    int strStr(string s1, string s2)
-    {
-        return s1.find(s2);
-    }
+vector<int> BuildLpsArray(string s)
+{
+	vector<int> lps;
+
+	int i = 0;
+	int len = 0;
+
+	lps.push_back(0);
+	i = 1;
+
+	while (i < s.size())
+	{
+		if (s[i] == s[len])
+		{
+			i++;
+			len++;
+			lps.push_back(len);
+		}
+		else
+		{
+			if (len != 0)
+			{
+				len = lps[len - 1];
+			}
+			else
+			{
+				len = 0;
+				lps.push_back(len);
+				i++;
+			}
+		}
+	}
+	return lps;
+}
+
+int Strstr(string s1, string s2, vector<int> lps)
+{
+	int i = 0;
+	int j = 0;
+    
+	while (i <= s1.size())
+	{
+		if (j == s2.size())
+		{
+			// Found a match
+			return (i - s2.size());
+		}
+
+		if (s1[i] == s2[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			if (j != 0)
+			{
+				j = lps[j - 1];
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
+    return -1;
+}
+    
+int strStr(string s1, string s2)
+{
+    if (s2 == "")
+        return 0;
+    else if (s1 == "")
+        return -1;
+        
+    vector<int> LpsArray = BuildLpsArray(s2);
+    return Strstr(s1, s2, LpsArray);
+}
 };
