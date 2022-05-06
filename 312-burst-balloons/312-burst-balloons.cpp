df62519410b71dd1b25f2bdb39da26c26dbafea0
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-06 20:50:17 +0530

    Time: 479 ms (59.38%), Space: 8.3 MB (92.92%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,28 +1,12 @@
+   
+
 class Solution
 {
     public:
     int cache[301][301];
     
     int maxCoins(vector<int>& nums)
-    {
-        // Cool intuition - https://www.youtube.com/watch?v=VFskby7lUbw
-        
-        // The main idea is we consider that each value is popped at the last.
-        
-        //        L        R
-        // Eg: 1 [3, 1, 5, 8] 1
-        // If we pop '3' at last, then maxCoins = (nums[L-1] x 3 x nums[R+1]) + DP[L+1, R]
-        //
-        //
-        // Assume 'i' is somewhere in the middle
-        //        L  i     R
-        // Eg: 1 [3, 1, 5, 8] 1
-        // Then maxSum if we pop i'th value at last = DP[L, i-1] + (nums[L-1] x 1 x nums[R+1]) + DP[i+1, R]
-        
-        // Time complexity: We assume that each value is popped at last        = O(n).
-        //                  And then we take all subarrays on the left & right = O(n^2).
-        //                                                           So, total = O(n^3).
-        
+    { 
         nums.insert(nums.begin(), 1);
         nums.insert(nums.end(), 1);
         int N = nums.size();
@@ -50,3 +34,23 @@ class Solution
         return cache[l][r];
     }
 };
+
+
+        // Cool intuition - https://www.youtube.com/watch?v=VFskby7lUbw
+        
+        // The main idea is we consider that each value is popped at the last.
+        
+        //        L        R
+        // Eg: 1 [3, 1, 5, 8] 1
+        // If we pop '3' at last, then maxCoins = (nums[L-1] x 3 x nums[R+1]) + DP[L+1, R]
+        //
+        //
+        // Assume 'i' is somewhere in the middle
+        //        L  i     R
+        // Eg: 1 [3, 1, 5, 8] 1
+        // Then maxSum if we pop i'th value at last = DP[L, i-1] + (nums[L-1] x 1 x nums[R+1]) + DP[i+1, R]
+        
+        // Time complexity: We assume that each value is popped at last        = O(n).
+        //                  And then we take all subarrays on the left & right = O(n^2).
+        //                                                           So, total = O(n^3).
+   
