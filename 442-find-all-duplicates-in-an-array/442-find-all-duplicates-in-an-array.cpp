3831fc627c301382c27ba7d5aebd52bf077adcb7
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-08 12:11:55 +0530

    Time: 63 ms (80.20%), Space: 33.5 MB (93.84%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -3,30 +3,16 @@ class Solution
     public:
     vector<int> findDuplicates(vector<int>& nums)
     {
+        vector<int> res;
         nums.push_back(0);
-        
-        vector<int> res;        
         for (int i=0; i<nums.size()-1; i++)
         {
             int index = abs(nums[i]);
-            
             if (nums[index] < 0)
-            {
                 res.push_back(index);
-            }
             else
-            {
-                if (nums[index] == 0)
-                    nums[index] = INT_MIN;
-                else
-                    nums[index] = -abs(nums[index]);
-            }
+                nums[index] = (nums[index] == 0)? INT_MIN: -abs(nums[index]);
         }
         return res;
     }
 };
-
-// [4, 3, 2, 7, 8, 2, 3, 1, 0]
-
-// [     -2,-7,-8,      -1,  ]
-// [0, 1, 2, 3, 4, 5, 6, 7, 8]
