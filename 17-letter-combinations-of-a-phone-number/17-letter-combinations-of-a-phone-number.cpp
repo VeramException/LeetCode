dca0f08fe15bed3549804741fd2afd46b155591e
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-09 10:08:52 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (20.65%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,17 +1,11 @@
 class Solution
 {
     public:
-    string digits;
-    unordered_map<char, vector<char>> m;
     vector<string> res;
-    
+    unordered_map<char, vector<char>> m;
+        
     vector<string> letterCombinations(string s)
     {
-        digits = s;
-        
-        if (digits == "")
-            return res;
-        
         m['2'] = {'a','b','c'};
         m['3'] = {'d','e','f'};
         m['4'] = {'g','h','i'};
@@ -21,19 +15,20 @@ class Solution
         m['8'] = {'t','u','v'};
         m['9'] = {'w','x','y','z'};
         
-        letterCombinationsUtil("", 0);
+        letterCombinationsUtil(s, 0, "");
+        
         return res;
     }
     
-    void letterCombinationsUtil(string prefix, int index)
+    void letterCombinationsUtil(string& s, int i, string currRes)
     {
-        if (index >= digits.size())
+        if (i == s.size() && currRes != "")
         {
-            res.push_back(prefix);
+            res.push_back(currRes);
             return;
         }
         
-        for (char c: m[digits[index]])
-            letterCombinationsUtil (prefix+c, index+1);
+        for (char c: m[s[i]])
+            letterCombinationsUtil(s, i+1, currRes+c);
     }
 };
