40dfeca27239516f7c5a7cfe64e4d87db3be6ff0
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-12 22:18:29 +0530

    Time: 79 ms (59.60%), Space: 38.5 MB (28.26%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -3,20 +3,20 @@ class Solution
     public:
     int numTimesAllBlue(vector<int>& flips)
     {
-        // 1         = 1
-        // 1+2       = 3
-        // 1+2+3     = 6
+        // 1         = 1 <-- expected sum at i=0
+        // 1+2       = 3 <-- expected sum at i=1
+        // 1+2+3     = 6 <-- expected sum at i=2
         
         int currSum = 0;
         int expectedSum = 0;
-        int count = 0;
+        int res = 0;
         for (int i=0; i<flips.size(); i++)
         {
             currSum += flips[i];
             expectedSum += (i+1);
             if (currSum == expectedSum)
-                count++;
+                res++;
         }
-        return count;
+        return res;
     }
 };
