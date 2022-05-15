a7c65a7e45c7fe3a0213a2e3de76e6c62f4923e2
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-15 17:46:08 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (78.39%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -14,12 +14,11 @@ class Solution
     public:
     bool isSameTree(TreeNode* p, TreeNode* q)
     {
-        if (p == nullptr && q == nullptr)
-            return true;
-        
-        if (p == nullptr || q == nullptr || p->val != q->val)
-            return false;
-        
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        if (p == nullptr || q == nullptr)
+            return p==q;
+
+        return p->val == q->val && 
+               isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
     }
 };
