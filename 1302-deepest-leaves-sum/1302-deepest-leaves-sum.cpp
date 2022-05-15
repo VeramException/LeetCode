4ceb46dd2946f0721783287b60fa6edf548a7af6
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-15 09:19:31 +0530

    Time: 127 ms (54.83%), Space: 59.7 MB (76.50%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    int deepest = 0;
+    int deepestSum = 0;
+    
+    int deepestLeavesSum(TreeNode* root)
+    {
+        deepestLeavesSumUtil(root, 0);
+        return deepestSum;
+    }
+    
+    void deepestLeavesSumUtil(TreeNode* node, int level)
+    {
+        if (node == nullptr)
+            return;
+        
+        if (deepest == level)
+            deepestSum += node->val;
+        else if (deepest < level)
+        {
+            deepest = level;
+            deepestSum = node->val;
+        }
+        
+        deepestLeavesSumUtil(node->left, level+1);
+        deepestLeavesSumUtil(node->right, level+1);
+    }
+};
