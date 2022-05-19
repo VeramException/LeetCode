eb017d5769249907f734c034ad33c562ee98d525
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-19 20:32:17 +0530

    Time: 76 ms (64.55%), Space: 38.4 MB (91.45%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution
 {
     public:
@@ -15,6 +16,27 @@ class Solution
             currSum += flips[i];
             expectedSum += (i+1);
             if (currSum == expectedSum)
+                res++;
+        }
+        return res;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    int numTimesAllBlue(vector<int>& flips)
+    {
+        int res = 0, rightMost = -1;
+        for (int i=0; i<flips.size(); i++)
+        {
+            // (i+1) ==> number of bulbs we turned on so far.
+            
+            if (flips[i] > rightMost)
+                rightMost = flips[i];
+            
+            if (rightMost == i+1)
                 res++;
         }
         return res;
