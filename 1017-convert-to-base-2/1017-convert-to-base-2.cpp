5c8b351da2b01a520ba94d8182bd8f0d7fa703c9
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-20 11:00:51 +0530

    Time: 7 ms (7.48%), Space: 6.1 MB (6.07%) - LeetHub

--- 1017-convert-to-base-2/1017-convert-to-base-2.cpp
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -4,7 +4,7 @@ class Solution
     string baseNeg2(int n)
     {
         string res = "";
-        while (n)
+        while (n != 0)
         {
             res = to_string(n & 1) + res;
             n = -(n >> 1);
