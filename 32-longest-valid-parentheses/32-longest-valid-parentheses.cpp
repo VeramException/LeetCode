d4997c9b66e62f7ece1953080f1b0b505672d80d
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-24 15:42:51 +0530

    Time: 12 ms (15.99%), Space: 7.3 MB (67.93%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -3,16 +3,17 @@ class Solution
     public:
     int longestValidParentheses(string s)
     {
+        int res = 0;
+        
         stack<int> st;
         st.push(-1);
         
-        int res = 0;
         for(int i=0; i<s.size(); i++)
         {
             if (s[i] == '(')
                 st.push(i);
             else
-            {
+            {   
                 st.pop();
                 if (st.empty())
                     st.push(i);
