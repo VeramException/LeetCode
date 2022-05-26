b599433114ddd76085714ce80626bf3d01a9e9d6
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-26 08:28:01 +0530

    Time: 5 ms (23.13%), Space: 5.8 MB (79.17%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,6 +3,12 @@ class Solution
     public:
     int hammingWeight(uint32_t n)
     {
+        // __builtin_popcount = int
+        // __builtin_popcountl = long int
+        // __builtin_popcountll = long long
+        
         return __builtin_popcount(n);
+        
+        //return bitset<32>(n).count();        
     }
 };
