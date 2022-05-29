30f75588c7945cb3bc272cd8d99520790725b3f4
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-29 13:04:14 +0530

    Time: 48 ms (16.94%), Space: 30 MB (5.99%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -3,28 +3,24 @@ class Solution
     public:    
     bool isSubtree(TreeNode* root, TreeNode* subRoot)
     {
-        string tree1;
-        string tree2;
-        
+        string tree1, tree2;        
         buildPreorder(root, tree1);
         buildPreorder(subRoot, tree2);
         
         if (tree1.find(tree2) == string::npos)
-            return false;
-        
+            return false;        
         return true;
     }
     
     void buildPreorder(TreeNode* root, string& s)
     {
         if (root == nullptr)
-        {
             s += ",#";
-            return;
+        else
+        {
+            s += "," + to_string(root->val);
+            buildPreorder(root->left, s);        
+            buildPreorder(root->right, s);
         }
-        
-        s += "," + to_string(root->val);
-        buildPreorder(root->left, s);        
-        buildPreorder(root->right, s);
     }
 };
