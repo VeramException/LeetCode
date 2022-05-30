656de96c929f0663b84b0922c00e5b271fcf48c3
VeramException <93516745+VeramException@users.noreply.github.com>
2022-05-30 15:16:05 +0530

    Time: 3 ms (67.24%), Space: 5.9 MB (30.45%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -6,6 +6,28 @@ class Solution
         if (origDividend == INT_MIN && origDivisor == -1)
             return INT_MAX;
         
-        return floor(origDividend/origDivisor);
+        bool negativeResult = ((origDividend<0)^(origDivisor<0));
+        
+        long int quotient = 0;
+        long int dividend = labs(origDividend);
+        long int divisor  = labs(origDivisor);
+
+        while (dividend-divisor > 0)
+        {
+            unsigned int x = -1;
+            while (dividend-divisor > 0)
+            {
+                x++;                
+                divisor = divisor<<1;
+            }
+            quotient += 1<<x;
+            dividend = dividend-(divisor>>1);
+            divisor  = labs(origDivisor);
+        }
+        
+        if (dividend-divisor == 0)
+            quotient++;
+        
+        return (negativeResult)? -quotient: quotient;
     }
 };
