ced92f05cc6a780f8d2bdd102bd4002c3904e2a0
VeramException <93516745+VeramException@users.noreply.github.com>
2022-06-04 13:08:15 +0530

    Time: 30 ms (25.72%), Space: 9.2 MB (28.43%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+    public:
+    int n;
+    unordered_set<int> cols;
+    unordered_set<int> posDiag;   // 'row+col' stays constant
+    unordered_set<int> negDiag;   // 'row-col' stays constant
+    
+    vector<vector<string>> res;
+    
+    vector<vector<string>> solveNQueens(int siz)
+    {
+        n = siz;
+        vector<string> board(n, string(n, '.'));
+        backtrack(0, board);
+        return res;
+    }
+    
+    void backtrack(int r, vector<string>& board)
+    {
+        if (r == n)
+        {
+            res.push_back(board);
+            return;
+        }
+        
+        for (int c=0; c<n; c++)
+        {
+            if (cols.count(c) || posDiag.count(r+c) || negDiag.count(r-c))
+                continue;
+            
+            cols.insert(c);
+            posDiag.insert(r+c);
+            negDiag.insert(r-c);            
+            board[r][c] = 'Q';
+            
+            backtrack(r+1, board);
+            
+            cols.erase(c);
+            posDiag.erase(r+c);
+            negDiag.erase(r-c);            
+            board[r][c] = '.';            
+        }
+    }
+};
