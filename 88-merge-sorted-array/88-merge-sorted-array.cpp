cde8f454ce693340350941b0a7c050861523c7cd
VeramException <93516745+VeramException@users.noreply.github.com>
2022-06-07 18:19:17 +0530

    Time: 6 ms (29.53%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -12,6 +12,7 @@ class Solution
             nums1[k--] = (nums1[i] > nums2[j])? nums1[i--]: nums2[j--];
         }
         
-        while (j >= 0) nums1[k--] = nums2[j--];
+        while (j >= 0) 
+            nums1[k--] = nums2[j--];
     }
 };
