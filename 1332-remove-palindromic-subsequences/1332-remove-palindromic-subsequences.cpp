fd92635124543612aaa2dd5f19d4aef023b668ae
VeramException <93516745+VeramException@users.noreply.github.com>
2022-06-08 10:57:11 +0530

    Time: 3 ms (39.03%), Space: 6.1 MB (78.83%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    int removePalindromeSub(string s)
+    {
+        if (s.empty())
+            return 0;
+        
+        return isPalindrome(s)? 1: 2;
+    }
+    
+    bool isPalindrome(string& s)
+    {
+        int l = 0, r = s.size()-1;
+        while (l <= r && s[l] == s[r])
+        {
+            l++;
+            r--;
+        }
+        return l>r;
+    }
+};
