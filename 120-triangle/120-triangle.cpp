c0fc27393884b1c8c635b340bc343fd21ce1cc5f
VeramException <93516745+VeramException@users.noreply.github.com>
2022-06-13 08:24:19 +0530

    Time: 12 ms (28.50%), Space: 8.5 MB (98.91%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -10,13 +10,10 @@ class Solution
             for (int j=0; j<m; j++)
             {
                 triangle[i][j] += min(triangle[i-1][max(0, j-1)], 
-                                          triangle[i-1][min(m-2, j)]);
-                
-                if (i == n-1)
-                    res = min (res, triangle[i][j]);
+                                      triangle[i-1][min(m-2, j)]);                
+                res = (i == n-1)? min (res, triangle[i][j]): INT_MAX;
             }
-        }
-        
+        }        
         return (n == 1)? triangle[0][0]: res;
     }
 };
