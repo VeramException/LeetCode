45734d7f138bf11e2067b4e39dd46aa0c42ab40b
VeramException <93516745+VeramException@users.noreply.github.com>
2022-06-17 11:05:32 +0530

    Time: 14 ms (68.31%), Space: 21.3 MB (55.06%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,35 @@
+#define NO_CAMERA       0
+#define HAS_CAMERA      2
+#define NOT_NEEDED      1
+
+class Solution
+{
+    public:
+    int res = 0;
+    
+    int dfs(TreeNode *root)
+    {
+        if (!root) 
+            return NOT_NEEDED;
+        
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        
+        if (l == NO_CAMERA || r == NO_CAMERA)
+        {
+            res++;
+            return HAS_CAMERA;
+        }
+        else if (l == HAS_CAMERA || r == HAS_CAMERA)
+            return NOT_NEEDED;
+        else
+            return NO_CAMERA;
+    }
+    
+    int minCameraCover(TreeNode* root)
+    {
+        if (dfs(root) == NO_CAMERA)
+            res++;
+        return res;
+    }
+};
